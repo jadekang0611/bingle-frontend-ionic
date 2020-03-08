3caaa3c92d1e6d14b4c410e96dcde1825ee306b5
jadekang <jkang.se@gmail.com>
2020-03-08 11:01:42 -0700

    logos for institutions are added and built the bone of the institutions component

--- src/pages/Institutions.tsx
+++ src/pages/Institutions.tsx
@@ -9,33 +9,34 @@ import {
   IonRow,
   IonCol
 } from '@ionic/react';
+import './Institutions.css';
 
 // Institution image are imported here
 
 import appAcademy from './image/appAcademy.png';
 import bloc from './image/bloc.png';
-import codingdojo from './image/codingdojo.png';
-import flatiron from './image/flatiron.png';
+import codingDojo from './image/codingDojo.png';
+import flatIron from './image/flatIron.png';
 import generalAssembly from './image/generalAssembly.png';
 import hackReactor from './image/hackReactor.png';
-import ironhack from './image/ironhack.png';
+import ironHack from './image/ironHack.png';
 import lambda from './image/lambda.png';
 import leWagon from './image/leWagon.png';
 import udacity from './image/udacity.png';
 
 const Institutions: React.FC = () => {
-  const institutionLogo = [
-    appAcademy,
-    bloc,
-    codingdojo,
-    flatiron,
-    generalAssembly,
-    hackReactor,
-    ironhack,
-    lambda,
-    leWagon,
-    udacity
-  ];
+  // const institutionLogo = [
+  //   appAcademy,
+  //   bloc,
+  //   codingdojo,
+  //   flatiron,
+  //   generalAssembly,
+  //   hackReactor,
+  //   ironhack,
+  //   lambda,
+  //   leWagon,
+  //   udacity
+  // ];
 
   return (
     <IonPage>
@@ -50,23 +51,62 @@ const Institutions: React.FC = () => {
             <IonTitle size="large">Institutions</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <br></br>
         <IonGrid>
-          <IonRow
-            {...institutionLogo.map(institution => {
-              return (
-                <IonCol size="6">
-                  <input
-                    type="image"
-                    src={institution}
-                    value={institution}
-                    width="150"
-                    height="150"
-                    alt="institution"
-                  />
-                </IonCol>
-              );
-            })}
-          ></IonRow>
+          <IonRow>
+            <IonCol className="institution-col">
+              <img src={appAcademy} alt={appAcademy} width="120" height="120" />
+            </IonCol>
+            <IonCol className="institution-col">
+              <img src={bloc} alt={bloc} width="120" height="120" />
+            </IonCol>
+          </IonRow>
+          <br></br>
+          <IonRow>
+            <IonCol className="institution-col">
+              <img src={codingDojo} alt={codingDojo} width="120" height="120" />
+            </IonCol>
+            <IonCol className="institution-col">
+              <img src={flatIron} alt={flatIron} width="120" height="120" />
+            </IonCol>
+          </IonRow>
+          <br></br>
+          <IonRow>
+            <IonCol className="institution-col">
+              <img
+                src={generalAssembly}
+                alt={generalAssembly}
+                width="120"
+                height="120"
+              />
+            </IonCol>
+            <IonCol className="institution-col">
+              <img
+                src={hackReactor}
+                alt={hackReactor}
+                width="120"
+                height="120"
+              />
+            </IonCol>
+          </IonRow>
+          <br></br>
+          <IonRow>
+            <IonCol className="institution-col">
+              <img src={ironHack} alt={ironHack} width="120" height="120" />
+            </IonCol>
+            <IonCol className="institution-col">
+              <img src={lambda} alt={lambda} width="120" height="120" />
+            </IonCol>
+          </IonRow>
+          <br></br>
+          <IonRow>
+            <IonCol className="institution-col">
+              <img src={leWagon} alt={leWagon} width="120" height="120" />
+            </IonCol>
+            <IonCol className="institution-col">
+              <img src={udacity} alt={udacity} width="120" height="120" />
+            </IonCol>
+          </IonRow>
         </IonGrid>
       </IonContent>
     </IonPage>
