06c8fe56d6ea508630b33bf375a4b61b573f6303
jadekang <jkang.se@gmail.com>
2020-03-10 12:37:21 -0700

    user view screen UI is 60% implemented and boxes are stacked correctly and button position is fixed

--- src/pages/UserView.tsx
+++ src/pages/UserView.tsx
@@ -11,7 +11,8 @@ import {
   IonCardTitle,
   IonButtons,
   IonButton,
-  IonBackButton
+  IonBackButton,
+  IonRippleEffect
 } from '@ionic/react';
 import React from 'react';
 import './UserView.css';
@@ -23,72 +24,95 @@ const UserView: React.FC = () => {
         <IonBackButton defaultHref="/landing" />
       </IonButtons>
       <IonContent>
-        <div className="behind-background">
+        <div id="behind-background-box">
           <img
             className="user-view-image"
             src="https://via.placeholder.com/200"
             alt="user"
           />
         </div>
-        <IonCard className="user-view-card">
-          <IonCardHeader>
-            <IonCardTitle>Mariah Carey</IonCardTitle>
-            <IonCardSubtitle>UI/UX Designer</IonCardSubtitle>
-            <IonCardSubtitle>
-              I became an expert in UI/UX after the GA immersive program!
-            </IonCardSubtitle>
-          </IonCardHeader>
-          <IonCardContent>
-            <IonButton>Follow</IonButton>
-            <IonGrid>
+        <div id="user-view-card-box">
+          <IonCard>
+            <IonCardHeader>
+              <IonCardTitle className="user-name">Mariah Carey</IonCardTitle>
+              <IonCardSubtitle className="user-name">
+                UI/UX Designer
+              </IonCardSubtitle>
+              <IonCardSubtitle className="user-name">
+                I became an expert in UI/UX after the GA immersive program!
+              </IonCardSubtitle>
+            </IonCardHeader>
+            <IonCardContent>
               <IonRow>
-                <IonCol>
-                  <IonRow>Followers</IonRow>
-                  <IonRow>20K</IonRow>
-                </IonCol>
-                <IonCol>
-                  <IonRow>Following</IonRow>
-                  <IonRow>5K</IonRow>
-                </IonCol>
+                <IonButton
+                  className="user-view-button ion-activatable ripple-parent"
+                  shape="round"
+                >
+                  {' '}
+                  Follow
+                  <IonRippleEffect type="unbounded"></IonRippleEffect>
+                </IonButton>
               </IonRow>
-            </IonGrid>
-          </IonCardContent>
-        </IonCard>
-        <IonGrid className="project-section">
-          <IonRow>
-            <IonCol>
-              <h2>PROJECTS</h2>
-            </IonCol>
-            <IonCol>
-              <IonButton>VIEW ALL</IonButton>
-            </IonCol>
-          </IonRow>
-          <IonRow>
-            <IonCol>
-              <img src="https://via.placeholder.com/150x120" alt="project" />
-            </IonCol>
-            <IonCol>
-              <img src="https://via.placeholder.com/150x120" alt="project" />
-            </IonCol>
-          </IonRow>
-          <IonRow>
-            <IonCol>
-              <img src="https://via.placeholder.com/150x120" alt="project" />
-            </IonCol>
-            <IonCol>
-              <img src="https://via.placeholder.com/150x120" alt="project" />
-            </IonCol>
-          </IonRow>
-          <IonRow>
-            <IonCol>
-              <img src="https://via.placeholder.com/150x120" alt="project" />
-            </IonCol>
-            <IonCol>
-              <img src="https://via.placeholder.com/150x120" alt="project" />
-            </IonCol>
-          </IonRow>
-          <IonButton className="contact-button">HIRE</IonButton>
-        </IonGrid>
+              <IonGrid>
+                <IonRow>
+                  <IonCol>
+                    <IonRow>Followers</IonRow>
+                    <IonRow>20K</IonRow>
+                  </IonCol>
+                  <IonCol>
+                    <IonRow>Following</IonRow>
+                    <IonRow>5K</IonRow>
+                  </IonCol>
+                </IonRow>
+              </IonGrid>
+            </IonCardContent>
+          </IonCard>
+        </div>
+        <div id="project-section-box">
+          <IonGrid>
+            <IonRow>
+              <IonCol>
+                <h2>PROJECTS</h2>
+              </IonCol>
+              <IonCol>
+                <IonButton
+                  className="user-view-button ion-activatable ripple-parent"
+                  shape="round"
+                >
+                  VIEW ALL
+                  <IonRippleEffect></IonRippleEffect>
+                </IonButton>
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol>
+                <img src="https://via.placeholder.com/150x120" alt="project" />
+              </IonCol>
+              <IonCol>
+                <img src="https://via.placeholder.com/150x120" alt="project" />
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol>
+                <img src="https://via.placeholder.com/150x120" alt="project" />
+              </IonCol>
+              <IonCol>
+                <img src="https://via.placeholder.com/150x120" alt="project" />
+              </IonCol>
+            </IonRow>
+            <IonRow>
+              <IonCol>
+                <img src="https://via.placeholder.com/150x120" alt="project" />
+              </IonCol>
+              <IonCol>
+                <img src="https://via.placeholder.com/150x120" alt="project" />
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </div>
+        <IonButton className="contact-button" shape="round">
+          HIRE
+        </IonButton>
       </IonContent>
     </IonPage>
   );
