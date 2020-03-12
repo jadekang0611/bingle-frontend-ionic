32a75e87246c25efc07f0cd6cc896fa61ec598bf
jadekang <jkang.se@gmail.com>
2020-03-12 02:37:12 -0700

    Show Followers and Following

--- src/pages/Follow.tsx
+++ src/pages/Follow.tsx
@@ -12,24 +12,140 @@ import {
   IonCol,
   IonChip
 } from '@ionic/react';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import {auth} from '../firebaseConfig';
 import { people } from './data/dummy-data';
 import { followingPeople } from './data/dummy-data-following';
 import './Follow.css';
 
+function useForceUpdate() {
+  const [value, setValue] = useState(0);
+  return () => setValue(value => ++value);
+}
+
 const Follow: React.FC = () => {
   let followers = people;
   let following = followingPeople;
-  const [showFollow, setShowFollow] = useState(followers);
+  const [showFollow, setShowFollow] = useState([{
+    name: "",
+    email: "",
+    blurb: "",
+    bootcamp: "",
+    completion: "",
+    title: "",
+    followers: "",
+    following: "",
+    github: "",
+    photo: "",
+    projects: ""
+  }]);
   const [who, setWho] = useState('Check out your followers');
+  let [currentFollowers, setCurrentFollowers] = useState([{
+    name: "",
+    email: "",
+    blurb: "",
+    bootcamp: "",
+    completion: "",
+    title: "",
+    followers: "",
+    following: "",
+    github: "",
+    photo: "",
+    projects: ""
+  }]);
+  
+  let [currentFollowing, setCurrentFollowing] = useState([{
+    name: "",
+    email: "",
+    blurb: "",
+    bootcamp: "",
+    completion: "",
+    title: "",
+    followers: "",
+    following: "",
+    github: "",
+    photo: "",
+    projects: ""
+  }]);
+
+
+  useEffect(() => {
+    if (auth !== null) {
+      if (auth.currentUser !== null) {
+        const uid = auth.currentUser.uid;
+
+        const params = {
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          method: 'GET'
+        };
+        fetch(
+          'https://us-central1-bingle-backend.cloudfunctions.net/app/api/read/followers/' +
+            uid,
+          params
+        )
+          .then(res => {
+            return res.json();
+
+          }).then(data => {
+            for (let i = 0; i < data.length; i++){
+              fetch(
+                'https://us-central1-bingle-backend.cloudfunctions.net/app/api/read/user/' +
+                  data[i],
+                params
+              )
+                .then(res => {
+                  return res.json();
+                })
+                .then(data => {
+                  let newFollowers = currentFollowers;                 
+                  newFollowers.push(data[0]);
+                  setCurrentFollowers(newFollowers);
+                  setShowFollow(newFollowers);
+                });             
+            }
+            console.log(currentFollowers);
+          })
+
+          // get following
+          fetch(
+            'https://us-central1-bingle-backend.cloudfunctions.net/app/api/read/following/' +
+              uid,
+            params
+          )
+            .then(res => {
+              return res.json();
+  
+            }).then(data => {
+              for (let i = 0; i < data.length; i++){
+                fetch(
+                  'https://us-central1-bingle-backend.cloudfunctions.net/app/api/read/user/' +
+                    data[i],
+                  params
+                )
+                  .then(res => {
+                    return res.json();
+                  })
+                  .then(data => {
+                    let newFollowing = currentFollowing;                 
+                    newFollowing.push(data[0]);
+                    setCurrentFollowing(newFollowing);
+                  });             
+              }
+              console.log(currentFollowing);
+            })
+      } // user is auth
+    }
+  }, []);
 
   const followerHandler = () => {
-    setShowFollow(followers);
+    setShowFollow(currentFollowers);
     setWho('Check out your followers');
   };
 
   const followingHandler = () => {
-    setShowFollow(following);
+    setShowFollow(currentFollowing);
     setWho('See who you are following');
   };
   return (
@@ -60,22 +176,23 @@ const Follow: React.FC = () => {
           <IonListHeader className="follow-main-title">{who}</IonListHeader>
           {showFollow.map((user, id) => {
             if (user !== undefined) {
+              if(user.bootcamp !== ""){
               return (
                 <IonItem key={id}>
                   <IonAvatar slot="start">
-                    <img src={user.img} alt={user.name} />
+                    <img src={user.photo} alt={user.name} />
                   </IonAvatar>
                   <IonLabel>
                     <IonRow className="follower-following-name-institution-row">
                       <h2>{user.name}</h2>
                       <IonChip className="institution-chip">
                         <IonLabel>
-                          <h3>{user.institution}</h3>
+                          <h3>{user.bootcamp}</h3>
                         </IonLabel>
                         <IonAvatar>
                           <img
-                            src={user.institutionLogo}
-                            alt={user.institution}
+                            src={user.photo}
+                            alt={user.bootcamp}
                           ></img>
                         </IonAvatar>
                       </IonChip>
@@ -84,6 +201,7 @@ const Follow: React.FC = () => {
                   </IonLabel>
                 </IonItem>
               );
+              }
             }
           })}
         </IonList>
